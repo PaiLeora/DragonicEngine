0ef44c7ea9026dd601005a6c056b5f1a68961634
PaiLeonore <fanatiksirpai@gmail.com>
2026-06-24 01:35:00 +0700

    Create Color.kt

---
+++ app/src/main/java/com/leonoretech/dragonicengine/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.leonoretech.dragonicengine.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BgBlack = Color(0xFF050505)
+val PanelDark = Color(0xFF0D0D0D)
+val BorderDim = Color(0xFF222222)
+val NeonCyan = Color(0xFF00F5FF)
+val NeonMagenta = Color(0xFFFF2BD6)
+val DangerRed = Color(0xFFFF3B3B)
+val TextPrimary = Color(0xFFEAEAEA)
+val TextDim = Color(0xFF8A8A8A)
